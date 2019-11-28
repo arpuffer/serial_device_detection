ca5c39bd5ad58882d36c5d20eadf2f7e3aaa0d8a
Alex Puffer <alex.puffer@gmail.com>
2019-11-28 14:39:11 -0800

    Function completed, responses added for testing purposes

--- serial_find.py
+++ serial_find.py
@@ -1,12 +1,26 @@
 from serial.tools import list_ports
-from typing import NamedTuple
+from serial.tools.list_ports_common import ListPortInfo
+from typing import NamedTuple, List
 
-class TargetDevice():
-    vid
-    pid
 
-def find_device(vid, pid):
-    ser_devices = list_ports()
-    # I don't have any USB to serial converters, so I can't tell what info PySerial can actually give on my machines
-    # TODO: Get a usb-to-serial converter
+def find_devices(vid: int, pid: int) -> List[ListPortInfo]:
+    """Return a list of serial devices that match given hardware id parameters
+    
+    Args:
+        vid (int): Vendor ID
+        pid (int): Product ID
+    
+    Returns:
+        List[ListPortInfo]
+    """
+    devices = list_ports.comports()
+    return [x for x in devices if all((x.vid==vid, x.pid==pid))]
 
+def main():
+    no_devices = find_devices(0, 0)
+    trendnet = find_devices(1659, 8963)
+    print("No device result: %s" % str(no_devices))
+    print("TRENDNet result:  %s" % str(trendnet))
+
+if __name__ == '__main__':
+    main()
